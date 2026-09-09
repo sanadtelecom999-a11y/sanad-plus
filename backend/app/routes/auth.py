30ad3811732a991094b6deef816b38f6019b0536
sanadtelecom999-a11y <sanadtelecom999@gmail.com>
2026-09-09 03:48:19 +0300

    Update auth.py

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,11 +1,11 @@
 import os
 import json
 import uuid
+from datetime import datetime, timezone
 from flask import request, jsonify
 from ..models.base import User
 from ..extensions import db
 from . import main
-from datetime import datetime, timezone
 
 def get_or_create_user(telegram_id, first_name="", last_name="", username=""):
     user = User.query.filter_by(telegram_id=telegram_id).first()
@@ -25,33 +25,49 @@ def get_or_create_user(telegram_id, first_name="", last_name="", username=""):
         )
         db.session.add(user)
         db.session.commit()
+    else:
+        # تحديث البيانات إذا تغيرت
+        changed = False
+        if user.first_name != first_name:
+            user.first_name = first_name
+            changed = True
+        if user.last_name != last_name:
+            user.last_name = last_name
+            changed = True
+        if user.username != username:
+            user.username = username
+            changed = True
+        if changed:
+            db.session.commit()
     return user
 
 @main.route("/api/auth/telegram", methods=["POST"])
 def telegram_auth():
     data = request.get_json()
-    init_data = data.get("initData", "")
-    if init_data:
-        # محاولة استخراج بيانات المستخدم من initData
-        try:
-            from urllib.parse import parse_qsl
-            params = dict(parse_qsl(init_data))
-            user_json = params.get("user", "{}")
-            user_data = json.loads(user_json)
-            telegram_id = user_data.get("id")
-            first_name = user_data.get("first_name", "")
-            last_name = user_data.get("last_name", "")
-            username = user_data.get("username", "")
-        except:
-            telegram_id = data.get("telegram_id")
-            first_name = data.get("first_name", "")
-            last_name = data.get("last_name", "")
-            username = data.get("username", "")
-    else:
-        telegram_id = data.get("telegram_id")
-        first_name = data.get("first_name", "")
-        last_name = data.get("last_name", "")
-        username = data.get("username", "")
+    if not data:
+        return jsonify({"error": "بيانات غير صالحة"}), 400
+
+    # استخراج البيانات من initData أو من الحقول المباشرة
+    telegram_id = data.get("telegram_id")
+    first_name = data.get("first_name", "")
+    last_name = data.get("last_name", "")
+    username = data.get("username", "")
+
+    # إذا لم يتوفر telegram_id، نحاول من initData
+    if not telegram_id:
+        init_data = data.get("initData", "")
+        if init_data:
+            try:
+                from urllib.parse import parse_qsl
+                params = dict(parse_qsl(init_data))
+                user_json = params.get("user", "{}")
+                user_data = json.loads(user_json)
+                telegram_id = user_data.get("id")
+                first_name = user_data.get("first_name", "")
+                last_name = user_data.get("last_name", "")
+                username = user_data.get("username", "")
+            except Exception:
+                pass
 
     if not telegram_id:
         return jsonify({"error": "telegram_id مطلوب"}), 400
